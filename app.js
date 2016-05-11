bf9bcda71583da9654ccf585a75d58ef1219a3b8
tamaraquinn <cupcake@london.com>
2016-05-11 17:27:52 +0100

    Commit Wednesday

---
+++ app.js
@@ -0,0 +1,39 @@
+var express = require("express");
+var app = express ();
+var bodyParser = require("body-parser");
+var csv = require("ya-csv");
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(express.static(__dirname + '/static'));
+
+app.get('/cupcake', function(request, response) {
+    response.send('Yummy Cupcakes');
+});
+
+app.get('/', function(request, response) {
+    response.sendFile(__dirname + "/index.html");
+});
+
+app.post('/signup', function(request, response) {
+    var tamara_name = request.body.visitorName;
+    var tamara_email = request.body.visitorEmail;
+    
+    var database = csv.createCsvFileWriter("signingup.csv", {flags: "a"});
+    console.log("Database" + database);
+    var data = [tamara_name,tamara_email];
+    database.writeRecord(data);
+    
+    database.writeStream.end();
+    
+    response.send("blah" + tamara_name);
+});
+
+app.listen(8000, function() {
+    console.log("Welcome");
+});
+
+
+
+
+
--- scripty.js
+++
@@ -1,4 +0,0 @@
-var greeting = 'Hi ';
-var name = prompt ("What's your name?");
-var greeting_message = greeting + name;
-alert (greeting_message)
